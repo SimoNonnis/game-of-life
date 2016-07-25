abf12d70c43e7420aa19532afdf65a5fbc48bba7
Simon Nonnis <simononnis@gmail.com>
2016-07-25 09:12:59 +0200

    'Add copy'

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # The game of life
 
 It is a 'cellular automaton', and was invented by Cambridge mathematician John Conway.
+
+The "game" is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves, or, for advanced "players", by creating patterns with particular properties.
--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,24 @@ import styles from './app.css';
 
 const App = () => (
   <div>
-      <h1 className={styles.title}>React + Webpack Boilerplate</h1>
-      <p>Let's start coding</p>
+    <h1 className={styles.title}>Game of Life</h1>
+
+    <p>The <strong>Game of Life</strong>, also known simply as <strong>Life</strong>, is a cellular automaton invented by the British mathematician John Horton Conway in 1970.</p>
+
+    <p>The "game" is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves, or, for advanced "players", by creating patterns with particular properties</p>
+
+    <h2>Rules</h2>
+
+    <p>The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, alive or dead.</p>
+
+    <p>Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:</p>
+
+    <ul>
+      <li>Each cell with less than 2 neighbors dies, for underpopulation</li>
+      <li>Each cell with more than 3 neighbors dies, as for overpopulation</li>
+      <li>Each cell with two or three neighbors survives</li>
+      <li>Each dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+    </ul>
   </div>
 );
 
