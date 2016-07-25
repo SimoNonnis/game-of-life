de6ae441a6802e4ff604b9bdd839b82164188437
Simon Nonnis <simononnis@gmail.com>
2016-07-26 00:38:51 +0200

    'Fix all errors'

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 
 // Import css
 import styles from './cell.css';
 
-//'dumb' functional component, only receives props. Don't need to dispatch actions nor do they care about the overall state of the app.
-const Cell = ({alive, newBorn, handleClick}) => (
-      <td
-        onClick={handleClick}
-        className={`${alive ? 'styles.alive' : ''} ${newBorn ? 'styles.new-born' : ''}`}
-        >
-      </td>
-);
+
+let cx = classNames.bind(styles);
+//'dumb' functional component, only receives props. Don't need to dispatch actions nor to they care about the overall state of the app.
+const Cell = ({alive, newBorn, handleClick}) => {
+  let cellClasses = cx({
+    alive: alive,
+    newBorn: newBorn
+  });
+
+  return (
+    <td
+      onClick={handleClick}
+      className={cellClasses}
+    >
+    </td>
+  );
+};
+
+
 
 
 Cell.propTypes = {
-  alive: React.PropTypes.bool,
-  newBorn: React.PropTypes.bool,
+  alive: React.PropTypes.any,
+  newBorn: React.PropTypes.any,
   handleClick: React.PropTypes.func
 };
 
